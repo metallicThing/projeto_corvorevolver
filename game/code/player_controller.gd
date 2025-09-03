fb2dfb29ef1538f984640c81fda59e00d160c5bb
Pedro <metalurgicthing@gmail.com>
2025-09-03 13:57:33 -0300

    commit n007
    adicionada movimentação básica do jogador
    - anda, pula e olha
    
    inclinações são a perdição da humanidade, meu ARCH NEMESIS que deve ser destruído

--- game/code/player_controller.gd
+++ game/code/player_controller.gd
@@ -4,12 +4,41 @@ const move_speed = 10
 const jump_strength = 15
 const gravity = 30
 
+@onready var camera = $camera_holder
+
+var input_dir
+var dir
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	
 	#ACTIONS#
-	if Input.is_action_just_pressed("space") and is_on_floor():
+	#JUMP#
+	if Input.is_action_just_pressed("space") and is_on_floor(): 
 		velocity.y = jump_strength
+	#DEBUG#
+	if Input.is_action_just_pressed("esc"):
+		get_tree().quit()
+	#DEBUG#
+	
+	#MOVE#
+	input_dir = Input.get_vector("a", "d", "w", "s")
+	dir = (transform.basis * Vector3(input_dir.x, 0, input_dir.y))
+	
+	if dir:
+		velocity.x = dir.x * move_speed
+		velocity.z = dir.z * move_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, move_speed)
+		velocity.z = move_toward(velocity.z, 0, move_speed)
 		
 	move_and_slide()
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(event.relative.x * -0.15))
+		camera.rotate_x(deg_to_rad(event.relative.y * -0.15))
+		
